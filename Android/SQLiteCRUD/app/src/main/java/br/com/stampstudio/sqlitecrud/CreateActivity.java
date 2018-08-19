d853939f7ec720bdbbcfa2c6b3188154efe21746
Pedro Teles <pedroateles@gmail.com>
2018-08-19 12:11:26 -0300

    Retrieve EditText data

--- Android/SQLiteCRUD/app/src/main/java/br/com/stampstudio/sqlitecrud/CreateActivity.java
+++ Android/SQLiteCRUD/app/src/main/java/br/com/stampstudio/sqlitecrud/CreateActivity.java
@@ -2,12 +2,49 @@ package br.com.stampstudio.sqlitecrud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class CreateActivity extends AppCompatActivity {
+import br.com.stampstudio.sqlitecrud.model.User;
+
+public class CreateActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText edtName;
+    private EditText edtAddress;
+    private EditText edtAge;
+    private EditText edtPhone;
+    private Button btnCreate;
+    private Button btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        edtName = findViewById(R.id.edtName);
+        edtAddress = findViewById(R.id.edtAddress);
+        edtAge = findViewById(R.id.edtAge);
+        edtPhone = findViewById(R.id.edtPhone);
+        btnCreate = findViewById(R.id.btnCreate);
+        btnCancel = findViewById(R.id.btnCancel);
+
+        btnCreate.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnCreate) {
+            User user = new User();
+
+            user.setName(edtName.getText().toString());
+            user.setAddress(edtAddress.getText().toString());
+            user.setAge(Integer.parseInt(edtAge.getText().toString()));
+            user.setTelephone(edtPhone.getText().toString());
+
+
+        } else if(view == btnCancel) {
+            finish();
+        }
     }
 }
