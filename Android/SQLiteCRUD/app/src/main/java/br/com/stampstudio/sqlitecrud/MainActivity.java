2ee1bbf7b1d773c9adfdb58bb9eb0e58e0dd0d08
Pedro Teles <pedroateles@gmail.com>
2018-08-19 11:56:18 -0300

    Add Create button functionality

--- Android/SQLiteCRUD/app/src/main/java/br/com/stampstudio/sqlitecrud/MainActivity.java
+++ Android/SQLiteCRUD/app/src/main/java/br/com/stampstudio/sqlitecrud/MainActivity.java
@@ -1,13 +1,43 @@
 package br.com.stampstudio.sqlitecrud;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnCreate;
+    private Button btnUpdate;
+    private Button btnDelete;
+    private Button btnList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCreate = findViewById(R.id.btnCreate);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnList = findViewById(R.id.btnList);
+
+        btnCreate.setOnClickListener(this);
+        btnUpdate.setOnClickListener(this);
+        btnDelete.setOnClickListener(this);
+        btnList.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+
+        if(view == btnCreate) {
+            intent = new Intent(MainActivity.this, CreateActivity.class);
+        }
+
+        if(intent != null) {
+            startActivity(intent);
+        }
     }
 }
